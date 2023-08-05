40b550a68926c3ed06728754b0e89d75c5815f7c
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-08-05 23:15:04 +0530

    adding avatar icons

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,12 +1,26 @@
+import { Avatar, AvatarFallback } from '@radix-ui/react-avatar';
 import { User } from 'next-auth';
-import { FC } from 'react';
+import { FC, use } from 'react';
+import Image from 'next/image';
+import { Icons } from './icons';
 
 interface UserAvatarProps {
     user: Pick<User, "name" | "image">
 }
 
 const UserAvatar: FC<UserAvatarProps> = ({ user }) => {
-    return <div>UserAvatar</div>
+    return <Avatar>
+        {user.image ? (
+            <div className='relative aspect-square h-full w-full'>
+                <Image fill src={user.image} alt="profile picture" referrerPolicy="no-referrer" />
+            </div>
+        ) : (
+            <AvatarFallback>
+                <span className='sr-only'>{user?.name}</span>
+                <Icons.user className='h-4 w-4' />
+            </AvatarFallback>
+        )}
+    </Avatar >
 }
 
 export default UserAvatar
