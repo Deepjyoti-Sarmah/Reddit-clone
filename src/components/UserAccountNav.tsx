702f312d6ca45e005167494f0992eb1235efbb6f
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-08-06 13:19:38 +0530

    adding user dropdownbox

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,8 +1,11 @@
+"use client"
 import { User } from 'next-auth';
-import { DropdownMenu, DropdownMenuTrigger } from './ui/DropdownMenu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from './ui/DropdownMenu';
 import { FC } from 'react';
 import UserAvatar from './UserAvatar';
-
+import { DropdownMenuItem, DropdownMenuSeparator } from '@radix-ui/react-dropdown-menu';
+import Link from 'next/link';
+import { signOut } from 'next-auth/react';
 
 interface UserAccountNavProps {
     user: Pick<User, "name" | "image" | "email">
@@ -19,7 +22,40 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
                 }} />
         </DropdownMenuTrigger>
 
-        
+        <DropdownMenuContent className='bg-white' align='end'>
+            <div className='flex item-center justify-start gap-2 p-2'>
+                <div className='flex flex-col space-y-1 leading-none'>
+                    {user.name && <p className='font-medium'>{user.name}</p>}
+                    {user.email && <p className='w-[200px] truncate text-sm text-zinc-700'>{user.email}</p>}
+                </div>
+            </div>
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem asChild>
+                <Link href='/'>Feed</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem asChild>
+                <Link href='/r/create'>Create community</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem asChild>
+                <Link href='/settings'>Setting</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem onSelect={(event) => {
+                event.preventDefault()
+                signOut({
+                    callbackUrl: `${window.location.origin}/sign-in`,
+                })
+            }} className='cursor-pointer'>
+                Sign out
+            </DropdownMenuItem>
+
+
+        </DropdownMenuContent>
     </DropdownMenu>
 }
 
